4198a8928e450916a72c33e537e88ef2db886396
diarra <mamediarra.ndiaye@epfedu.fr>
2026-03-03 21:31:06 +0100

    Ajout chart et carrousel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,16 +3,12 @@ import { Home } from './home/home';
 import { MoviesList } from './movies-list/movies-list';
 import { AddMovie } from './add-movie/add-movie';
 import { EditMovie } from './edit-movie/edit-movie';
-import { Login } from './auth/login/login';
-import { Register } from './auth/register/register';
-import { authGuard } from './auth/auth.guard';
+import { Charts } from './charts/charts';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'movies', component: MoviesList },
   { path: 'add-movie', component: AddMovie },
   { path: 'edit-movie/:id', component: EditMovie },
-  { path: 'login', component: Login },
-  { path: 'register', component: Register }
-
+  { path: 'charts', component: Charts }
 ];
