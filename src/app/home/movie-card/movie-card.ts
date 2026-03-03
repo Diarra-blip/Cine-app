4c580cc8bd2fef2b3b9009612661a5f35ed1935e
diarra <mamediarra.ndiaye@epfedu.fr>
2026-03-03 14:05:10 +0100

    Initial commit - Structure propre

--- src/app/home/movie-card/movie-card.ts
+++ src/app/home/movie-card/movie-card.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
-import { Movie } from '../../models/movie';
+import { CommonModule } from '@angular/common';
+import { Movie } from '../../models/movie'; // Vérifie que le chemin vers ton modèle est correct
 
 @Component({
   selector: 'app-movie-card',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './movie-card.html',
   styleUrl: './movie-card.scss'
 })
 export class MovieCard {
-  @Input() movie!: Movie;
+  @Input({ required: true }) movie!: Movie;
 }
