e9f588f1ecabac776063d93d65acdb29a7bfe2c8
sonja_dia <sonja.dia@epfedu.fr>
2026-03-03 16:24:12 +0100

    Explications

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,16 @@ import { Home } from './home/home';
 import { MoviesList } from './movies-list/movies-list';
 import { AddMovie } from './add-movie/add-movie';
 import { EditMovie } from './edit-movie/edit-movie';
+import { Login } from './auth/login/login';
+import { Register } from './auth/register/register';
+import { authGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'movies', component: MoviesList },
   { path: 'add-movie', component: AddMovie },
-  { path: 'edit-movie/:id', component: EditMovie }
+  { path: 'edit-movie/:id', component: EditMovie },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register }
+
 ];
--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,13 +1,38 @@
+// src/app/navbar/navbar.ts
 import { Component, Input } from '@angular/core';
-import { TitleCasePipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+// import { ThemeService } from '../theme-service/theme-service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [TitleCasePipe, RouterLink],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.scss',
+  styleUrls: ['./navbar.scss'],
 })
 export class Navbar {
   @Input({ required: true }) title!: string;
+
+  expanded = false;   // toggle par clic
+  hoverMenu = false;  // toggle par hover
+
+  // Injection du service Auth
+  constructor(public auth: AuthService /*, public themeService: ThemeService */) {}
+
+  // Toggle menu hamburger
+  toggleMenu(): void {
+    this.expanded = !this.expanded;
+  }
+
+  // Méthode pour savoir si la navbar est "expand"
+  isExpanded(): boolean {
+    return this.expanded || this.hoverMenu;
+  }
+
+  // Toggle thème si tu actives ThemeService
+  // toggleTheme(): void {
+  //   this.themeService.toggleTheme();
+  // }
 }
